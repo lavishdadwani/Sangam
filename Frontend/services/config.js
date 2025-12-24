2158827fe8baa467d517abc96e7fbe99b5f4f6ab
LAVISH DADWANI <79633523+lavishdadwani@users.noreply.github.com>
2025-12-25 02:25:02 +0530

    Update config.js

--- Frontend/services/config.js
+++ Frontend/services/config.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import { create } from "apisauce"
 
 const customAxiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/auth"
+    // baseURL: "http://localhost:8000/api/auth"
+    baseURL: "https://sangam-h96y.onrender.com/api/auth"
 })
 
 const client = create({axiosInstance:customAxiosInstance})
