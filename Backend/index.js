1e5080c4f33260ef8a4889d5c47f65e96e52e2a2
Lavish Dadwani <lavishdadwanii@gmail.com>
2025-12-25 03:39:50 +0530

    added other methods in cors

--- Backend/index.js
+++ Backend/index.js
@@ -29,7 +29,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
-    methods:["POST", "GET"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
